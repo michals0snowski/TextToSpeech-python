8537c54154d9d24dabc96f401f20cb73b2a7e807
michals0snowski <109098947+michals0snowski@users.noreply.github.com>
2024-01-16 14:43:55 +0100

    Add files via upload

---
+++ txtToSpeech.py
@@ -0,0 +1,22 @@
+from gtts import gTTS
+import os
+from tkinter import *
+
+root = Tk()
+canvas = Canvas(root, width=400, height=300)
+canvas.pack()
+
+def textToSpeech():
+    text = entry.get()
+    language='en'
+    output = gTTS(text=text,lang=language,slow=False)
+    output.save('output.mp3')
+    os.system("start output.mp3") #afplay for mac
+
+entry = Entry(root)
+canvas.create_window(200,180,window=entry)
+
+button = Button(text="Start",command=textToSpeech)
+canvas.create_window(200,230,window=button)
+
+root.mainloop()
